a97c4e80a33e62180742be9e08e4eb642fecf259
Mathius Johnson <devachievem@gmail.com>
2020-07-31 12:29:16 -0700

    forgot to save other file

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const breedDetailsFromFile = function(breed, done) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("this is an error: ", error);
-    console.log("this is data: ", data);
+
     console.log("In readFile's Callback: it has the data.");
-    // console.log("this is breed: ", breed);
     if (error) {
       done(undefined);
     }
